8e4f9886a3b6047168f092b2e9419b2bb5faedd0
Linkka <samilindroos@hotmail.com>
2016-01-26 00:45:33 +0200

    Initial commit.

---
+++ Kotitehtava5olio20012016/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava5olio20012016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a one car instance
+            Radio testi1 = new Radio();
+            testi1.OnOff = true;
+            testi1.Frequency = 2000.0;
+            testi1.Volume = 1;
+            // display car data
+            testi1.PrintData();
+            Console.ReadLine();
+            // accelerate, change car's speed value
+            testi1.Change();
+            // display car data again, notice changed speed value!
+            testi1.PrintData();
+            testi1.Change();
+            Console.ReadLine();
+            testi1.PrintData();
+            Console.ReadLine();
+            testi1.Change();
+            testi1.PrintData();
+            Console.ReadLine();
+            testi1.Change();
+            testi1.PrintData();
+            Console.ReadLine();
+            testi1.Change();
+            testi1.PrintData();
+            Console.ReadLine();
+            testi1.Change();
+        }
+    }
+}
---
+++ Kotitehtava5olio20012016/Radio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava5olio20012016
+{
+    class Radio
+    {
+        private readonly double MinFrequency = 2000.0;
+        private readonly double MaxFrequency = 26000.0;
+        private readonly int MinVolume = 0;
+        private readonly int MaxVolume = 9;
+        public bool OnOff { get; set; }
+
+        private double frequency;
+        private int volume;
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (value <= MaxFrequency && value >= MinFrequency) frequency = value;
+                else
+                {
+                    Console.WriteLine("Frequency too high or low. Choose one between 2000.0 and 26000.0 - set to minimum!");
+                    frequency = MinFrequency;
+                }
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= MaxVolume && value >= MinVolume) volume = value;
+                else
+                {
+                    Console.WriteLine("Volume too high or low. Choose one between 0 and 9 - set to minimum!");
+                    volume = MinVolume;
+                }
+            }
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Radio data : ");
+            Console.WriteLine("- onoff : " + OnOff);
+            Console.WriteLine("- frequency : " + Frequency);
+            Console.WriteLine("- volume : " + Volume);
+        }
+        public void Change()
+        {
+            Frequency += 10000.0;
+            Volume += 4;
+        }
+    }
+}
